4ec43917803d8ccd547309d85c16262d9d0d0ce5
mannp <mannp@users.noreply.github.com>
2022-05-13 14:50:19 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -139,13 +139,13 @@ variable "k3s_disable_components" {
 }
   
 variable "k3s_extra_server_args" {
-  description = "List of components to disable. Ref: https://rancher.com/docs/k3s/latest/en/installation/install-options/server-config/"
+  description = "List of additional server arguements. Ref: https://rancher.com/docs/k3s/latest/en/installation/install-options/server-config/"
   type        = list(string)
   default     = []
 }
 
 variable "k3s_extra_worker_args" {
-  description = "List of components to disable. Ref: https://rancher.com/docs/k3s/latest/en/installation/install-options/agent-config/"
+  description = "List of additional worker arguements. Ref: https://rancher.com/docs/k3s/latest/en/installation/install-options/agent-config/"
   type        = list(string)
   default     = []
 }
